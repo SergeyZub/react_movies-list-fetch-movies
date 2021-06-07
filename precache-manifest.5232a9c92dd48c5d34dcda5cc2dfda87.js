10e25d58f354a07ddab92c052f5ecaa940c523ab
Sergey Zub <vook1996@gmail.com>
2021-06-07 14:28:27 +0300

    Updates

---
+++ precache-manifest.5232a9c92dd48c5d34dcda5cc2dfda87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b35b866c4968c0a780d018c70b2f87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d63e28c6593eea7ae541",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "cabf1902c82cacaed015",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "d63e28c6593eea7ae541",
+    "url": "./static/js/2.05dd1c1d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.05dd1c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cabf1902c82cacaed015",
+    "url": "./static/js/main.4d6cc4f2.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
